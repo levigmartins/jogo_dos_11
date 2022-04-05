0682f96b105b07f902510ca215844fb88e655d4d
Levi Martins <levi.gm99@gmail.com>
2022-04-05 20:21:59 -0300

    Tela "Em breve"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.PUBLIC_URL + "/brasil_futebol.svg"} className="App-logo" alt="logo" />
+        <p className='title'>
+          Jogo dos Onze
+        </p>      
+          Em breve
       </header>
     </div>
   );
