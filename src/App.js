d7744a0140ec8d33699e32f6358a5a62a8e53a01
Levi Martins <levi.gm99@gmail.com>
2022-06-07 13:52:51 -0300

    Field background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={process.env.PUBLIC_URL + "/brasil_futebol.svg"} className="App-logo" alt="logo" />
-        <p className='title'>
-          Jogo dos Onze
-        </p>      
-          Em breve
-      </header>
-    </div>
-  );
+
+  const [render, setRender] = useState(false);
+  
+  if(!render)
+    return (
+      <div className="App">
+        <header className="App-header">            
+          <div onClick={() => setRender(true)}>
+            <img src={process.env.PUBLIC_URL + "/brasil_futebol.svg"} className="App-logo" alt="logo" />
+          </div>
+            <p className='title'>
+              Jogo dos Onze
+            </p>      
+              Em breve
+        </header>
+      </div>
+    );
+  else
+      return(
+        <div className="App">
+        <header className="App-header" style={{perspective: '1000px'}}>
+            <img src={process.env.PUBLIC_URL + "/field.svg"} className="Field" alt="logo" />
+        </header>
+      </div>
+      )
 }
 
 export default App;
